0bf06c22dc6e776d03589035064ca0e4ed922c9c
lucmi <lucmi418@gmail.com>
2017-02-07 21:38:37 +1300

    Add data models

---
+++ ToDoList/Model/ToDoItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ToDoList.Model
+{
+    class ToDoItem
+    {
+        private DateTime? _dueDate;
+        private string _name;
+        private DateTime? _dateCompleted;
+        private DateTime _dateCreated;
+
+        public DateTime? DueDate
+        {
+            get { return _dueDate; }
+            set { _dueDate = value; }
+        }
+
+        public string Name
+        {
+            get { return Name; }
+            set { _name = value; }
+        }
+
+        public bool Complete
+        {
+            get
+            {
+                return _dateCompleted != null;
+            }
+
+            set
+            {
+                if(value)
+                {
+                    _dateCompleted = DateTime.Now;
+                }
+                else
+                {
+                    _dateCompleted = null;
+                }
+            }
+        }
+
+        public ToDoItem(string name)
+        {
+            _dateCreated = DateTime.Now;
+            Name = name;
+        }
+
+        public ToDoItem(string name, DateTime dueDate)
+        {
+            _dateCreated = DateTime.Now;
+            Name = name;
+            DueDate = dueDate;
+        }
+    }
+}
---
+++ ToDoList/Model/ToDoList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ToDoList.Model
+{
+    class ToDoList
+    {
+        private string _name;
+        private DateTime _created;
+        private ObservableCollection<ToDoItem> _items;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DateTime Created
+        {
+            get { return _created; }
+        }
+
+        public ObservableCollection<ToDoItem> Items
+        {
+            set { _items = value; }
+            get
+            {
+                if (_items == null)
+                {
+                    _items = new ObservableCollection<ToDoItem>();
+                }
+                return _items;
+            }
+        }
+    }
+}
